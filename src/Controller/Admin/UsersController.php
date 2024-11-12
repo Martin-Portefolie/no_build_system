17042fb526cb4bb2bfcacc669cf9eced250c1042
Mr-Martin-Kristiansen <krimart@aarhus.dk>
2024-11-12 12:39:58 +0100

    added users

--- src/Controller/Admin/UsersController.php
+++ src/Controller/Admin/UsersController.php
@@ -3,6 +3,7 @@
 namespace App\Controller\Admin;
 
 use App\Entity\Client;
+use App\Entity\Team;
 use App\Entity\User;
 use App\Form\ClientType;
 use App\Form\UserType;
@@ -10,59 +11,119 @@ use Doctrine\ORM\EntityManagerInterface;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\PasswordHasher\Hasher\UserPasswordHasherInterface;
 use Symfony\Component\Routing\Attribute\Route;
+
 
 class UsersController extends AbstractController
 {
+    private EntityManagerInterface $entityManager;
+    private UserPasswordHasherInterface $passwordHasher;
 
-    private $entityManager;
-    public function __construct(EntityManagerInterface $entityManager)
+    public function __construct(EntityManagerInterface $entityManager, UserPasswordHasherInterface $passwordHasher)
     {
         $this->entityManager = $entityManager;
+        $this->passwordHasher = $passwordHasher;
     }
 
-    #[Route('/users', name: 'admin_users')]
+    #[Route('/admin/user', name: 'admin_users')]
     public function index(): Response
     {
-        $userdata = $this->entityManager->getRepository(User::class)->findAll();
+        $users = $this->entityManager->getRepository(User::class)->findAll();
+        $allTeams = $this->entityManager->getRepository(Team::class)->findAll();
 
-        foreach ($userdata as $user){
+        $userDataArray = [];
+        foreach ($users as $user) {
             $userDataArray[] = [
                 'id' => $user->getId(),
                 'name' => $user->getUsername(),
                 'email' => $user->getEmail(),
                 'password' => $user->getPassword(),
-                'team' => $user->getTeams(),
-                'timelog' => $user->getTimelogs(),
-                'role' => $user->getRoles(),
-
+                'teams' => $user->getTeams(),
+                'roles' => $user->getRoles(),
             ];
         }
 
-
         return $this->render('admin/users/index.html.twig', [
-            'controller_name' => 'UsersController',
-            'users' => $userDataArray
-            ]);
+            'userDataArray' => $userDataArray,
+            'allTeams' => $allTeams,
+        ]);
     }
-    #[Route('/user/new', name: 'admin_user_post', methods: ['POST'])]
-    public function new(Request $request) : Response
+
+    #[Route('/admin/user/new', name: 'admin_user_new')]
+    public function new(Request $request): Response
     {
         $user = new User();
         $form = $this->createForm(UserType::class, $user);
-
         $form->handleRequest($request);
 
         if ($form->isSubmitted() && $form->isValid()) {
+            // Set default role if no roles are assigned
+            if (empty($user->getRoles())) {
+                $user->setRoles(['ROLE_USER']);
+            }
+
+            // Hash password
+            $plainPassword = $form->get('password')->getData();
+            if ($plainPassword) {
+                $hashedPassword = $this->passwordHasher->hashPassword($user, $plainPassword);
+                $user->setPassword($hashedPassword);
+            }
+
             $this->entityManager->persist($user);
             $this->entityManager->flush();
 
-            return $this->redirectToRoute('admin_user');
+            return $this->redirectToRoute('admin_users');
         }
 
-        return $this->render('admin/client/new.html.twig', [
+        return $this->render('admin/users/new.html.twig', [
             'form' => $form->createView(),
         ]);
-
     }
+
+    #[Route('/admin/user/update/{id}', name: 'admin_user_update', methods: ['POST'])]
+    public function update(Request $request, int $id, UserPasswordHasherInterface $passwordHasher): Response
+    {
+        $user = $this->entityManager->getRepository(User::class)->find($id);
+
+        if (!$user) {
+            throw $this->createNotFoundException('User not found');
+        }
+
+        // Update basic fields
+        $user->setUsername($request->request->get('name'));
+        $user->setEmail($request->request->get('email'));
+
+        // Update roles
+        $roles = $request->request->all('roles'); // Use all() for array data
+        $user->setRoles(!empty($roles) ? $roles : $user->getRoles());
+
+        // Update password if provided
+        $newPassword = $request->request->get('new_password');
+        if (!empty($newPassword)) {
+            $hashedPassword = $passwordHasher->hashPassword($user, $newPassword);
+            $user->setPassword($hashedPassword);
+        }
+
+        // Update team associations
+        $selectedTeamIds = $request->request->all('team_ids'); // Use all() for array data
+        foreach ($user->getTeams() as $team) {
+            if (!in_array($team->getId(), $selectedTeamIds)) {
+                $user->removeTeam($team);
+            }
+        }
+        foreach ($selectedTeamIds as $teamId) {
+            $team = $this->entityManager->getRepository(Team::class)->find($teamId);
+            if ($team && !$user->getTeams()->contains($team)) {
+                $user->addTeam($team);
+            }
+        }
+
+        $this->entityManager->flush();
+
+        return $this->redirectToRoute('admin_users');
+    }
+
+
+
 }
--- src/Form/UserType.php
+++ src/Form/UserType.php
@@ -6,26 +6,37 @@ use App\Entity\Team;
 use App\Entity\User;
 use Symfony\Bridge\Doctrine\Form\Type\EntityType;
 use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\ChoiceType;
 use Symfony\Component\Form\FormBuilderInterface;
 use Symfony\Component\OptionsResolver\OptionsResolver;
 
 class UserType extends AbstractType
 {
-
-
     public function buildForm(FormBuilderInterface $builder, array $options): void
     {
         $builder
-            ->add('email')
-            ->add('roles')
-            ->add('password')
             ->add('username')
+            ->add('email')
+            ->add('password')
+            ->add('roles', ChoiceType::class, [
+                'choices' => [
+                    'User' => 'ROLE_USER',
+                    'Admin' => 'ROLE_ADMIN',
+                ],
+                'expanded' => true,
+                'multiple' => true,
+                'label' => 'Roles',
+                'required' => false,
+                'empty_data' => 'ROLE_USER', // default role
+            ])
             ->add('teams', EntityType::class, [
                 'class' => Team::class,
-                'choice_label' => 'id',
+                'choice_label' => 'name', // Or any other property you want to display as the label
                 'multiple' => true,
-            ])
-        ;
+                'expanded' => true, // Set to true if you want checkboxes; false for a multi-select dropdown
+            ]);
+
     }
 
     public function configureOptions(OptionsResolver $resolver): void
